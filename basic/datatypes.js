3e9b82cdcb5a0beac0d781fa29b738bca547d262
Saharukh <saharukhshaikh460@gmail.com>
2024-03-26 18:26:59 +0100

    added basic

---
+++ basic/datatypes.js
@@ -0,0 +1,21 @@
+//Primitive 
+
+// 7types : String, number, boolean, null, undefined, symbol, BigInt
+
+
+//Reference(Non- primitive)
+
+//Array, objects, functions
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId) 
+
+// // this is not equal bcz evrytime symbol will give different data
+// console.log(typeof id)
+
+const number = 3
+const anotherNumber = String(number)
+console.log(anotherNumber)
+console.log(typeof anotherNumber)
