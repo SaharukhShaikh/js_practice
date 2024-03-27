2639ebedef47fb19ad7bd547977ab6401106fc28
Saharukh <saharukhshaikh460@gmail.com>
2024-03-27 18:16:34 +0100

    added array.js

---
+++ basic/array.js
@@ -0,0 +1,16 @@
+//array methods
+
+let myArray = [0,1,2,3,4]
+console.log(myArray[1]);
+
+// myArray.push(5,'simran')
+// console.log(myArray);
+
+// myArray.pop()
+// console.log(myArray);
+
+// myArray.unshift(2)
+// console.log(myArray);
+
+// myArray.shift()
+// console.log(myArray);
---
+++ basic/datetime.js
@@ -0,0 +1,8 @@
+// let myDate = new Date()
+// console.log(myDate.toString());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getMonth());
+console.log(newDate.getTimezone());
