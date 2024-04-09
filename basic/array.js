e89228d5283a548f88a82561454267075143b048
Saharukh <saharukhshaikh460@gmail.com>
2024-04-09 12:25:48 +0200

    objects

--- basic/array.js
+++ basic/array.js
@@ -1,7 +1,7 @@
 //array methods
 
-let myArray = [0,1,2,3,4]
-console.log(myArray[1]);
+// let myArray = [0,1,2,3,4]
+// console.log(myArray[1]);
 
 // myArray.push(5,'simran')
 // console.log(myArray);
@@ -14,3 +14,23 @@ console.log(myArray[1]);
 
 // myArray.shift()
 // console.log(myArray);
+
+
+//CONCAT - you can add only one value
+
+// const one = ["ADcdscs","acdscc"]
+// const two = ["sadsf","dcsv"]
+// const all = one.concat(two)
+// console.log(all);
+
+//spread- same as concat but here you can add multiple values
+
+// const one = ["ADcdscs","acdscc"]
+// const two = ["sadsf","dcsv"]
+
+// const all = [...one,...two]
+// console.log(all);
+
+//flat - to find how many elements are there in that array
+const one = ["ADcdscs","acdscc",[2,23,3,3],[23,333,4566]]
+// cont new = one.flat();
---
+++ basic/objects.js
@@ -0,0 +1,27 @@
+// const mySym= Symbol("asd")
+
+// const JsUser={
+//     name:"saharukh",
+//     "full name":"saharukh shaikh",
+//     email:"cr7.saharukh@gmail.com",
+//     [mySym]:"mynama"
+
+// }
+
+// // JsUser.email="saharukhshaikh460@gmail.com"
+
+// // console.log(JsUser["full name"]);
+// Object.freeze(JsUser)//to freeze all the element in a object
+// JsUser.email="saharukhshaikh460@gmail.com"
+// console.log(JsUser);
+
+
+//Two types of object singleton and non-singleton object
+//singleton object
+
+// const anyUser= new Object()
+
+//non-singleton object
+// const anyUser = {}
+// console.log(anyUser);
+
